8f0e4834ad803411d05c3411f18e141eac9eaafe
Victor Coutinho <victorkclic@hotmail.com>
2023-03-28 20:33:55 -0300

    test generate

--- app/Http/Controllers/GenerateController.php
+++ app/Http/Controllers/GenerateController.php
@@ -15,26 +15,6 @@ class GenerateController extends Controller
     }
 
     public function generateQrCode(Request $request){
-        $name = $request->input('name');
-        $linkedin = $request->input('linkedin');
-        $github = $request->input('github');
-        $msg = '';
-        $profileExists = Profile::where('name', $name)->first();
-        if (!$profileExists) {
-            $profile = new Profile;
-            $profile->name = $name;
-            $profile->linkedin = $linkedin;
-            $profile->github = $github;
-            $profile->save();
-        }else{
-            $msg = 'Profile already exists.';
-        }
-        
-        $options = new QROptions([
-            'version' => 5,
-            'outputType' => QRCode::OUTPUT_IMAGE_PNG,
-            'eccLevel' => QRCode::ECC_L,
-        ]);
         
         if ($msg!=''){
             return view('generate', ['qrcode' => $content, 'name' => $name, 'msg'=> 'Profile already exists.']);
