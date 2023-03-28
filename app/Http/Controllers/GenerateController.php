d3023604ff9373cc70b6f3742082ec267d7aab81
Victor Coutinho <victorkclic@hotmail.com>
2023-03-28 20:48:04 -0300

    test generate name route

--- app/Http/Controllers/GenerateController.php
+++ app/Http/Controllers/GenerateController.php
@@ -15,7 +15,30 @@ class GenerateController extends Controller
     }
 
     public function generateQrCode(Request $request){
+        $name = $request->input('name');
+        $linkedin = $request->input('linkedin');
+        $github = $request->input('github');
+        $msg = '';
+        $profileExists = Profile::where('name', $name)->first();
+        if (!$profileExists) {
+            $profile = new Profile;
+            $profile->name = $name;
+            $profile->linkedin = $linkedin;
+            $profile->github = $github;
+            $profile->save();
+        }else{
+            $msg = 'Profile already exists.';
+        }
         
+        $options = new QROptions([
+            'version' => 5,
+            'outputType' => QRCode::OUTPUT_IMAGE_PNG,
+            'eccLevel' => QRCode::ECC_L,
+        ]);
+        $outputFile = "qrcodes/$name.png";
+        $qrCode = new QRCode($options);
+        $qrCode->render("$name", $outputFile);
+        $content = (string)$outputFile;
         if ($msg!=''){
             return view('generate', ['qrcode' => $content, 'name' => $name, 'msg'=> 'Profile already exists.']);
         }else{
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,6 @@ Route::get('/', function () {
 Route::get('/', [HomeController::class, 'index'])->name('/');
 
 Route::get('/generate', [GenerateController::class, 'index'])->name('generateIndex');
-Route::post('/generate', [GenerateController::class, 'generateQrCode']);
+Route::post('/generate', [GenerateController::class, 'generateQrCode'])->name('generateQrCode');
 
 Route::get('/profile/{name}', [ProfileController::class, 'index']);
