01f0d230dc429d89ad849c19822d55e5325d3f99
Victor Coutinho <victorkclic@hotmail.com>
2023-03-28 21:13:58 -0300

    dd in generate controller

--- public/phpinfo.php
+++ public/phpinfo.php
@@ -1,1 +1,8 @@
-<?php phpinfo(); ?>
+<?php
+    // Verificando se a extensão GD está habilitada
+    if (extension_loaded('gd') && function_exists('gd_info')) {
+        echo "A extensão GD está habilitada.";
+    } else {
+        echo "A extensão GD não está habilitada.";
+    }
+?>
